beb7e1e0d153ebc96128a8d706492154a8185293
Andrew Shinjo <shinjo1233@gmail.com>
2021-10-06 23:06:10 -0700

    user can input x and y coordinate

--- tictactoe.asm
+++ tictactoe.asm
@@ -6,20 +6,45 @@
 	newLine: .asciiz "\n"
 	xPrompt: .asciiz "X: "
 	yPrompt: .asciiz "Y: "
-	board: .byte  ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
+	board: .byte  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'
 .text
 	main:
-		# Print xPrompt
-		li $v0, 4
-		la $a0, xPrompt
+		# Get x input from the user, and store it in $t0
+		la $a0, xPrompt		# $a0 equals "X: "
+		jal printString		# Prints $a0
+		jal getIntegerInput	# $v0 equals X input from user
+		move $t0, $v0		# $t0 equals $v0
+		
+		# Get y input from the user, and store it in $t1
+		la $a0, yPrompt		# $a0 equals "Y: "
+		jal printString		# Prints string stored in $a0
+		jal getIntegerInput	# $v0 equals Y input from user
+		move $t1, $v0		# $t1 equals $v0
+		
+		# Print (x, y)
+		li $a0, '('		
+		li $v0, 11		
+		syscall			
+		move $a0, $t0		
+		jal printInteger	
+		li $a0, ','	
+		li $v0, 11	
+		syscall
+		move $a0, $t1
+		jal printInteger
+		li $a0, ')'
+		li $v0, 11
 		syscall
 		
-		# Read integer input
-		li $v0, 5
-		syscall
+		la $a0, newLine
+		jal printString
 		
-		move $a0, $v0
-		li $v0, 1
+		addi $t2, $zero, 3
+		mul $t2, $t0, $t2
+		add $t2, $t2, $t1
+		
+		li $v0, 11
+		lb $a0, board($t2)
 		syscall
 		
 		
@@ -27,6 +52,20 @@
 		li $v0, 10
 		syscall
 	
+	printInteger:
+		li $v0, 1		# Sets syscall to print the integer stored in $a0
+		syscall			# Prints integer stored in $a0
+		jr $ra			# Returns to main
+	
+	printString:
+		li $v0, 4		# Sets syscall to print the string stored in $a0
+		syscall			# Prints string stored in $a0
+		jr $ra			# Returns to main
+	
+	getIntegerInput:
+		li $v0, 5		# Sets syscall to get an integer input from the user
+		syscall			# $v0 equals integer input from the user
+		jr $ra 			# Returns to main
 	drawBoard:
 		li $v0, 4
 		la $a0, xAxis
